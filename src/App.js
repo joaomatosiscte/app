fc9afe7d097ee2c4fc98df480622625880d900a6
João Matos <joao@bklyn.co>
2023-11-06 17:34:31 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  AppConfig,
+  UserSession,
+  AuthDetails,
+  showConnect,
+} from "@stacks/connect";
+import { useState, useEffect } from "react";
+import { userSession } from './auth';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Users from "./pages/Users";
+import Movie from "./pages/Movie";
+import Footer from './components/Footer';
+
 function App() {
+  const [userData, setUserData] = useState(undefined);
+
+  useEffect(() => {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        setUserData(userData);
+      });
+    } else if (userSession.isUserSignedIn()) {
+      setUserData(userSession.loadUserData());
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/movie/:id" element={<Movie />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
+    
   );
 }
 
